268d44829df53c653a3b920d7b13d9ec72cf5d1d
soumengit43 <soumen.das.43@gmail.com>
2025-08-19 09:45:34 +0000

    Remove unrachable code junit dependency added

--- src/main/java/org/auth/validationService.java
+++ src/main/java/org/auth/validationService.java
@@ -23,35 +23,23 @@ public class validationService {
 
     // This method is for validate only one condition among rest of the 3 condition
     public static void validateOneCondition(String password) throws InvalidPassException{
-            int trueCount = 1;
-            int check = 0;
+            int trueCount = 0;
             // 1. Password char length check
-            if (password.length() < 8) {
-                check = 1;
-            }else {
+            if (password.length() < 8)
                 trueCount++;
-            }
             // 3. Uppercase check using Regex
-            if (!password.matches(".*[A-Z].*")) {
-                check = 2;
-            }else {
+            if (!password.matches(".*[A-Z].*"))
                 trueCount++;
-            }
             // 5. Number check using Regex
-            if (!password.matches(".*[0-9].*")) {
-                check = 3;
-            } else{
+            if (password.matches(".*[0-9].*"))
                 trueCount++;
-            }
-            // Below first 2 nested if conditions are unreachable due to question No 2.
-            // Q1.2 Null/Empty/Whitespace character and Q1.4 are must conditions. Only one condition among rest of 3 conditions will satisfy the question No 2.
-            if(trueCount<2){
-                if(check == 1)
-                    throw new InvalidPassException("Password should be larger than 8 chars ");
-                if(check == 2)
-                    throw new InvalidPassException("Password should have at least one uppercase letter");
-                if(check == 3)
-                    throw new InvalidPassException("Password should have at least one number \n or one uppercase letter \n or larger than 8 chars");
+
+             if(trueCount < 1){
+                throw new InvalidPassException("\nPassword must satisfy at least ONE of the following:\n" +
+                        "Minimum length of 8 Char\n"+
+                        "At least one uppercase letter \n"+
+                        "At least one digit"
+                );
             }
     }
 
